ef23556ec349d44a78ba34d437b9b1b12324342d
Anibal <git@anibalcopitan.com>
2025-11-26 10:59:29 -0500

    update: I scaled my laptop screen to 16:9 = 1600x900

---
+++ share-screen-x11vnc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# Share screen for live
+#
+
+# x11vnc -clip 960x1080+1440+0 -cursor most -pointer_mode 1 -nocursorshape -noxfixes -scale_cursor 3
+x11vnc -clip 960x1080+1600+0 -cursor most -pointer_mode 1 -nocursorshape -noxfixes -scale_cursor 3
